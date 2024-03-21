55b300d8037169994b022a0e6ce239355ec7e479
dymarcovitz <dymarcovitz@gmail.com>
2024-03-21 11:52:40 +0200

    my first commit in vscode

---
+++ try_vscode.py
@@ -0,0 +1,20 @@
+import torch
+import pandas as pd
+import numpy as np
+
+print(torch.__version__)
+
+a=5
+
+print(f'this {a}')
+
+def myfunc(a):
+    b=a+5
+    return b
+
+c=myfunc(7)
+print(c)
+
+a=pd.DataFrame(np.random.randint(low=0,high=9,size=(5,8)))
+
+print(a)
